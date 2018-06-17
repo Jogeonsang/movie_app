27b22b82283a47563cc5345f72846b25800044d1
Jogeonsang <rhcn258@gmail.com>
2018-06-17 16:07:42 +0900

    .

--- src/App.js
+++ src/App.js
@@ -9,32 +9,7 @@ class App extends Component {
   }
 
 componentDidMount(){
-  setTimeout(() => {
-    this.setState({
-      movies: [
-        {
-          title: "Age of Ultron",
-          poster: "http://www.impawards.com/2015/posters/avengers_age_of_ultron_ver23.jpg"
-        },
-        {
-          title: "I want to eat your pancreas",
-          poster: "http://img.tenasia.hankyung.com/webwp_kr/wp-content/uploads/2017/09/2017091909152517879-540x756.jpg"
-        },
-        {
-          title: "DeadPool",
-          poster: "https://ext.fmkorea.com/files/attach/new/20160202/86431577/3579565/305192604/123c9da040ca24fd7918ff3d5a1e9d9c.jpg"
-        },
-        {
-          title: "Black Panther",
-          poster: "http://image.chosun.com/sitedata/image/201801/18/2018011800526_0.jpg"
-        },
-        {
-          title: "YAYAYA",
-          poster: "http://img.tenasia.hankyung.com/webwp_kr/wp-content/uploads/2017/09/2017091909152517879-540x756.jpg"
-        }
-      ]
-    })
-  }, 2000 )
+  fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
 }
 
 _renderMovies = () => {
