69f17fbc00d6e63039cc61497aa4c1ecff3705b0
Jogeonsang <rhcn258@gmail.com>
2018-06-18 00:07:19 +0900

    css추가와 App.js, Movie.js 정리 완료

--- src/App.js
+++ src/App.js
@@ -1,28 +1,53 @@
-import React, { Component } from 'react';
-import './App.css';
-import Movie from './Movie';
+import React, { Component } from "react";
+import "./App.css";
+import Movie from "./Movie";
 
 class App extends Component {
+  // Render: componentWillMount() -> render() -> componentDidMount()
+  // Update componentWillReceiveProps() -> shouldComponentUpdate() -> componentWillUpdate() -> render() -> componentDidUpdate()
 
-  state = {
+  state = {};
 
+  componentDidMount() {
+    this._getMovies();
   }
 
-componentDidMount(){
-  fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
-}
+  _renderMovies = () => {
+    const movies = this.state.movies.map(movie => {
+      return (
+        <Movie
+          title={movie.title_english}
+          poster={movie.medium_cover_image}
+          key={movie.id}
+          genres={movie.genres}
+          synopsis={movie.synopsis}
+        />
+      );
+    });
+    return movies;
+  };
 
-_renderMovies = () => {
-  const movies = this.state.movies.map((movie, index) => {
-    return <Movie title={movie.title} poster={movie.poster} key={index} />
-  })
-  return movies
-}
+  _getMovies = async () => {
+    const movies = await this._callApi();
+    this.setState({
+      movies
+    });
+  };
+
+  _callApi = () => {
+    return fetch(
+      "https://yts.am/api/v2/list_movies.json?sort_by=download_count"
+    )
+      .then(potato => potato.json())
+      .then(json => json.data.movies)
+      .catch(err => console.log(err));
+  };
 
   render() {
+    const { movies } = this.state;
     return (
-      <div className="App">
-        {this.state.movies ? this._renderMovies() : 'Loding'}
+      <div className={movies ? "App" : "App--loading"}>
+        {movies ? this._renderMovies() : "Loading"}
       </div>
     );
   }
